5a00296f240be15c5ce0924843a3a0307a753035
OussamaBrikScolab <39066801+OussamaBrikScolab@users.noreply.github.com>
2020-07-22 23:44:00 -0400

    Add files via upload

--- js/NameGenerator.js
+++ js/NameGenerator.js
@@ -1,14 +1,10 @@
 var vowels = ["a", "e", "i", "o", "u", "y"];
 var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z"];
-var wlen = 2;
 
 function PickRandomWord(frm,wlen) {
   var m = "";
   var x = 0;
-  if(wlen == null){
-    alert('you didnt set any value for the length of generated word, the default value will be 2 characters');
-    var wlen = 2;
-} 
+
   while (x < wlen) {
      if (odd(x) == 1) {
        m += vowels[random(0, vowels.length-1)];
